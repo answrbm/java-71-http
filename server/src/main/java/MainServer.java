45e87f698625e193216d52d72205cebccd9839b4
Ansar Bektassov <bektasovansar@gmail.com>
2024-04-22 09:42:57 +0500

    Changed communication between servers

--- server/src/main/java/MainServer.java
+++ server/src/main/java/MainServer.java
@@ -4,6 +4,10 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class MainServer {
 
@@ -15,9 +19,30 @@ public class MainServer {
 
     private static void handleProducts(HttpExchange exchange) throws IOException {
         if(exchange.getRequestMethod().equals("GET")) {
-            exchange.sendResponseHeaders(200,0);
-            try(OutputStream os = exchange.getResponseBody()) {
-                os.write("Products list".getBytes());
+
+            HttpClient client = HttpClient.newHttpClient();
+
+            URI uri = URI.create("http://localhost:7070/products/stats");
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(uri)
+                    .build();
+
+            HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+
+            try {
+                HttpResponse<String> response = client.send(request, handler);
+
+                System.out.println("Response code: " + response.statusCode());
+                System.out.println("Response body: " + response.body());
+
+                exchange.sendResponseHeaders(200,0);
+                try(OutputStream os = exchange.getResponseBody()) {
+                    os.write(response.body().getBytes());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         } else {
             exchange.sendResponseHeaders(405,0);
--- stat-server/src/main/java/StatServer.java
+++ stat-server/src/main/java/StatServer.java
@@ -4,12 +4,10 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 public class StatServer {
+
+    private static int counter;
 
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(7070),0);
@@ -19,30 +17,10 @@ public class StatServer {
 
     private static void handleProductsStats(HttpExchange exchange) throws IOException {
         if(exchange.getRequestMethod().equals("GET")) {
-
-            HttpClient client = HttpClient.newHttpClient();
-
-            URI uri = URI.create("http://localhost:8080/products");
-
-            HttpRequest request = HttpRequest.newBuilder()
-                    .GET()
-                    .uri(uri)
-                    .build();
-
-            HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
-
-            try {
-                HttpResponse<String> response = client.send(request, handler);
-
-                System.out.println("Response code: " + response.statusCode());
-                System.out.println("Response body: " + response.body());
-
-                exchange.sendResponseHeaders(200,0);
-                try(OutputStream os = exchange.getResponseBody()) {
-                    os.write(response.body().getBytes());
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            counter++;
+            exchange.sendResponseHeaders(200,0);
+            try(OutputStream os = exchange.getResponseBody()) {
+                os.write(("Visitors count: " + counter).getBytes());
             }
         } else {
             exchange.sendResponseHeaders(405,0);
@@ -50,9 +28,6 @@ public class StatServer {
                 os.write("Method not allowed".getBytes());
             }
         }
-
-
-
     }
 
 }
